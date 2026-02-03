27b03aa61d69853e9826e26f720b1e9542af5429
kimseunggyun0 <skun620@naver.com>
2026-02-03 23:21:21 +0900

    first commit

---
+++ src/main/java/com/plibrary/pokelibrary/PokeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.plibrary.pokelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeLibraryApplication.class, args);
+    }
+
+}
